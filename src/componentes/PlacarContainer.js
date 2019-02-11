aa8f05532bf4a6dbd9e4d853d014763fad284c04
= <jardriel.redes18@gmail.com>
2019-02-10 22:42:20 -0300

    Refatorando componentes

--- src/componentes/PlacarContainer.js
+++ src/componentes/PlacarContainer.js
@@ -23,21 +23,21 @@ export default class PlacarContainer extends React.Component {
 	}
 
 	render(){
+		const {partida , casa , visitante} = this.props;
+		const estilo = { float:"left","marginRight":"20px"};
         return  (
         	<div>
-        		<div style={{ float:"left","marginRight":"20px"}}>
+        		<div style={estilo}>
 					<h3>Casa</h3>
-					<Time nome={this.props.casa.nome} gols={this.state.golsCasa}
+					<Time nome={casa.nome} gols={this.state.golsCasa}
 					marcarGol={this.MarcarGolsCasa.bind(this)}/>
 				</div>
-        		<div style={{ float:"left","marginRight":"20px"}}>
-					<Partida estadio={this.props.partida.estadio}
-					data={this.props.partida.data}
-					horario={this.props.partida.horario}/>
+        		<div style={estilo}>
+					<Partida {...partida} />
 				</div>
-        		<div style={{ float:"left","marginRight":"20px"}}>
+        		<div style={estilo}>
 				<h3>Visitante</h3>
-				<Time nome={this.props.visitante.nome} gols={this.state.golsVisitante}
+				<Time nome={visitante.nome} gols={this.state.golsVisitante}
 				marcarGol={this.MarcarGolsVisitante.bind(this)}/>
 				</div>
 				<div style={{ clear:"both"}}></div>
